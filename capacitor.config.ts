13818ed422930450b2e5cb175eb218a49e697350
fernando-ruans <fernando_ruans@hotmail.com>
2025-04-24 09:17:56 -0300

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,7 +7,7 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: "http",
     // URL atualizada para o domínio correto
-    url: "http://10.0.0.12:5000",
+    url: "http://10.0.0.12:10000",
     cleartext: true
   },
   plugins: {
